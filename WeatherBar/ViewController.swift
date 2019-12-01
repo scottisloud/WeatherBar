96aa0d86934053691500d6812a5eac64c58fc09d
Scott Lougheed <scott@scottlougheed.com>
2019-12-01 13:14:56 -0500

    Initial Commit

---
+++ WeatherBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherBar
+//
+//  Created by Scott Lougheed on 2019/12/1.
+//  Copyright © 2019 Scott Lougheed. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
